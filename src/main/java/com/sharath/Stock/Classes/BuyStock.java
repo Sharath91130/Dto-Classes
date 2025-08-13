a1738c0cdc425b89434277a9fd9a631d0e8a63ac
Sharath91130 <sharathkurer8055@gmail.com>
2025-08-13 18:05:19 +0530

    added

---
+++ src/main/java/com/sharath/Stock/Classes/BuyStock.java
@@ -0,0 +1,28 @@
+package com.sharath.Stock.Classes;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class BuyStock {
+
+
+	private Integer id;
+
+	private String symbol;
+
+	private Integer quantity;
+
+
+	private Double totalPrice;
+
+	private Integer userId;
+
+
+	private LocalDateTime createTime;
+
+	private String status;
+}
+
+
+
